201b17e701393b9297d6e6806597024a53e75556
Javier Cañadilla Casco <jcanadillacasco@outlook.com>
2021-07-11 21:47:45 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = {
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+
+        floors.forEach((floor) => {
+            // Listen to button pressed
+            floor.on("up_button_pressed", function() {
+                // Maybe tell an elevator to go to this floor?
+                console.log('FLOOR UP PRESSED?', this.level);
+            });
+            floor.on("down_button_pressed", function() {
+                // Maybe tell an elevator to go to this floor?
+                console.log('FLOOR DOWN PRESSED?', this.level);
+            })
+        })
+       
+        // Whenever the elevator is idle (has no more queued destinations) ...
+        elevator.on("idle", function() {
+            // let's go to all the floors (or did we forget one?)
+            const currentFloor = elevator.currentFloor();
+            console.log("CURRENT FLOOR: ", currentFloor);
+            if (currentFloor === 0) {
+                elevator.goingUpIndicator(true);
+            } else if (floors.length - 1 === currentFloor) {
+                elevator.goingDownIndicator(false);
+            }
+
+            console.log("DIRECTION: ", elevator.destinationDirection());
+
+            const pressedFloors = elevator.getPressedFloors();
+            console.log("PRESSED FLOORS: ", pressedFloors)
+
+            // Get floors selected
+
+            if (pressedFloors.length > 0) {
+                pressedFloors.forEach((floor) => {
+                    console.log("Go to floor: ", floor);
+                    elevator.goToFloor(floor);
+                });                
+            }
+
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    },
+}
